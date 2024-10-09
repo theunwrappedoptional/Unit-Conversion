ad87d54b9e81fb254e88041ceb32eb8503b60731
Manhattan <carra.rules@gmail.com>
2024-10-09 11:08:58 +0200

    Initial Commit

---
+++ Unit Conversion/Unit_ConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit_ConversionApp.swift
+//  Unit Conversion
+//
+//  Created by Manhattan on 09/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Unit_ConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
