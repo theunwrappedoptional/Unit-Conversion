a35aa451e9cde52f840de2da2b4887209bc2aca7
Manhattan <carra.rules@gmail.com>
2024-10-16 11:56:13 +0200

    Complete LengthView

--- Unit Conversion/LengthView.swift
+++ Unit Conversion/LengthView.swift
@@ -8,11 +8,75 @@
 import SwiftUI
 
 struct LengthView: View {
+    
+    @FocusState private var valueIsFocused: Bool
+    @State private var valueToConvert: Double = 0
+    
+    let lengthsDict:[String: UnitLength] = [
+        "Meters": UnitLength.meters,
+        "Kilometers": UnitLength.kilometers,
+        "Feet": UnitLength.feet,
+        "Yards": UnitLength.yards,
+        "Miles": UnitLength.miles
+    ]
+
+    @State private var unitToConvert = "Meters"
+    @State private var unitConverted = "Yards"
+    
+    var convertedValue: Double{
+        let input = Measurement(value: valueToConvert, unit: lengthsDict[unitToConvert]!)
+        let output = input.converted(to: lengthsDict[unitConverted]!)
+        return output.value
+    }
+    
     var body: some View {
-        Text("Length")
+        NavigationStack{
+            Form{
+                Section("Length in \(unitToConvert)") {
+                    TextField("Value to convert", value: $valueToConvert, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($valueIsFocused)
+                    LenghtUnitSelector(unit:$unitToConvert)
+                }
+                
+                Section("Length in \(unitConverted)") {
+                    Text(String(format: "%.2f", convertedValue))
+                    LenghtUnitSelector(unit:$unitConverted)
+                }
+            }
+            .navigationTitle("📏 Length")
+            .toolbar {
+                if valueIsFocused{
+                    Button("Done") {
+                        valueIsFocused = false
+                    }
+                }
+            }
+        }
     }
 }
 
 #Preview {
     LengthView()
+}
+
+struct LenghtUnitSelector: View {
+    
+    @Binding var unit: String
+    let lengths:[String] = [
+        "Meters",
+        "Kilometers",
+        "Feet",
+        "Yards",
+        "Miles"
+    ]
+    
+    var body: some View {
+        Picker("Length Unit", selection: $unit) {
+            ForEach(lengths, id: \.self) {
+                Text("\($0)")
+            }
+        }
+        .pickerStyle(.segmented)
+    }
 }
